f70a02c86f537274b96c043f6226d3109da2fac8
Hugo Ceniceros Olave <hcenicerosolave@gmail.com>
2026-03-26 14:56:47 +0100

    feat: add initial Express application with a basic reflected input vulnerability.

--- app/app.js
+++ app/app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// Secreto hardcodeado a propósito
-const SLACK_BOT_TOKEN = "xoxb-123456789012-1234567890123-4567890123456-a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6";
+
 
 app.get('/', (req, res) => {
     // Vulnerabilidad básica reflejando input sin sanitizar
