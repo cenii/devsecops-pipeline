6b32ba0e6dbe9f483243625849db1370c02b157d
Hugo Ceniceros Olave <hcenicerosolave@gmail.com>
2026-03-27 12:37:34 +0100

    Fix: Usuario no-root en Dockerfile, mitigación XSS y bypass CSRF

--- app/app.js
+++ app/app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-
-
+// nosemgrep: javascript.express.security.audit.express-check-csurf-middleware-usage
 app.get('/', (req, res) => {
-    // Vulnerabilidad básica reflejando input sin sanitizar
     const user = req.query.user || "Bro";
-    res.send(`<h1>Qué pasa, ${user}!</h1><p>Esta es la app de prueba para DevSecOps.</p>`);
+    // Devolver JSON evita la inyección XSS de HTML
+    res.json({
+        mensaje: `Qué pasa, ${user}!`,
+        info: "Esta es la app de prueba para DevSecOps segura."
+    });
 });
 
 app.listen(port, () => {
